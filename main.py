5c10b4f060d84367f183be85728242e324612043
Mothercrystal <thomas.dieden@gmx.de>
2025-02-13 17:49:41 +0100

    turning left and right

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
+
         screen.fill("black")
-
-        
         player.draw(screen)
-
         pygame.display.flip()
 
 
