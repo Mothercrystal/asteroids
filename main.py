9adf6816aaebae37f0b012b47d9ae9b68728a9f3
Mothercrystal <thomas.dieden@gmx.de>
2025-02-13 19:30:15 +0100

    chapter 2 done, player done

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from player import *
 
 def main():
     pygame.init()
-
     clock = pygame.time.Clock()
     dt = 0
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
 
@@ -18,10 +23,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
 
